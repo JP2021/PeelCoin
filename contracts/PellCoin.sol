b08d9b2106e003cbc1f00eea2da2acbc47167c30
jpdss2020 <joaopaulo.atm@gmail.com>
2024-07-23 10:49:21 -0300

    Update

--- contracts/PellCoin.sol
+++ contracts/PellCoin.sol
@@ -1,25 +1,19 @@
-//SPDX-License-Identifier: MIT
-// Author: João Paulo de São Severino
+// SPDX-License-Identifier: MIT
+// Author - João Paulo de São Severino
 
 pragma solidity ^0.8.7;
 
-contract PellCoin {
-    uint256 private _totalSupply = 100000000 * 10**18;
+contract DizzyCoin {
+    uint256 private _totalSupply = 482000000000000 * 10**18;
     string private _name = "PellCoins";
-    string private _symbol = "Pell";
+    string private _symbol = "PELL";
     uint8 private _decimals = 18;
 
     mapping(address => uint256) private _balances;
-
     mapping(address => mapping(address => uint256)) private _allowances;
 
     event Transfer(address indexed from, address indexed to, uint256 value);
-
-    event Approval(
-        address indexed owner,
-        address indexed spender,
-        uint256 value
-    );
+    event Approval(address indexed owner, address indexed spender, uint256 value);
 
     constructor() {
         _balances[msg.sender] = _totalSupply;
@@ -42,7 +36,8 @@ contract PellCoin {
     }
 
     function transfer(address to, uint256 value) public returns (bool) {
-        require(balanceOf(msg.sender) >= value, "Insuficient balance.");
+        require(value > 0, "Transfer value must be greater than 0.");
+        require(_balances[msg.sender] >= value, "Insufficient balance.");
         _balances[to] += value;
         _balances[msg.sender] -= value;
         emit Transfer(msg.sender, to, value);
@@ -55,20 +50,31 @@ contract PellCoin {
         return true;
     }
 
-    function transferFrom(
-        address from,
-        address to,
-        uint256 value
-    ) public returns (bool) {
-        require(balanceOf(from) >= value, "Insufficient balance.");
-        require(
-            _allowances[from][msg.sender] >= value,
-            "Insufficient allowance."
-        );
+    function transferFrom(address from, address to, uint256 value) public returns (bool) {
+        require(value > 0, "Transfer value must be greater than 0.");
+        require(_balances[from] >= value, "Insufficient balance.");
+        require(_allowances[from][msg.sender] >= value, "Insufficient allowance.");
         _balances[to] += value;
         _balances[from] -= value;
         _allowances[from][msg.sender] -= value;
         emit Transfer(from, to, value);
+        return true;
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply;
+    }
+
+    function increaseAllowance(address spender, uint256 addedValue) public returns (bool) {
+        _allowances[msg.sender][spender] += addedValue;
+        emit Approval(msg.sender, spender, _allowances[msg.sender][spender]);
+        return true;
+    }
+
+    function decreaseAllowance(address spender, uint256 subtractedValue) public returns (bool) {
+        require(_allowances[msg.sender][spender] >= subtractedValue, "Decreased allowance below zero");
+        _allowances[msg.sender][spender] -= subtractedValue;
+        emit Approval(msg.sender, spender, _allowances[msg.sender][spender]);
         return true;
     }
 }
